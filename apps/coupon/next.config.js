1108c718ce2285d03fc25ece934a8f9b327da3b6
Jordan Rosenberg <1338914+Joroze@users.noreply.github.com>
2021-12-26 22:51:18 +0100

    Add in new project, update deps

---
+++ apps/coupon/next.config.js
@@ -0,0 +1,43 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withNx = require('@nrwl/next/plugins/with-nx');
+
+/**
+ * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  nx: {
+    // Set this to true if you would like to to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
+  },
+  swcMinify: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    domains: ['images.ctfassets.net', 's.gravatar.com'],
+  },
+  // eslint: {
+  //   // Warning: Dangerously allow production builds to successfully complete even if
+  //   // your project has ESLint errors.
+  //   ignoreDuringBuilds: true,
+  // },
+  async redirects() {
+    return [
+      {
+        source: '/test',
+        destination: 'https://www.coinbase.com/join/rosenb_826',
+        permanent: true,
+      },
+    ];
+  },
+  experimental: {
+    // concurrentFeatures: true,
+    // serverComponents: true,
+  },
+  future: {
+    strictPostcssConfiguration: true,
+  },
+};
+
+module.exports = withNx(nextConfig);
