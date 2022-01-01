79c0e27be7a36aa288f1e3a35b00831233e04c57
Jordan Rosenberg <1338914+Joroze@users.noreply.github.com>
2022-01-01 22:16:27 +0100

    fix

--- apps/coupon/pages/_app.tsx
+++ apps/coupon/pages/_app.tsx
@@ -1,10 +1,11 @@
 import { AppProps } from 'next/app';
 import './styles.scss';
-import type { GetStaticPropsContext, NextPage } from 'next';
+import type { NextPage } from 'next';
 import type { ReactElement, ReactNode } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Theme } from '@joroze/ui';
 import Layout from '../layouts/Layout';
+import Head from 'next/head';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -19,6 +20,29 @@ function CustomApp({ Component, pageProps }: AppPropsWithLayout) {
 
   return (
     <ChakraProvider resetCSS theme={Theme}>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       {getLayout(<Component {...pageProps} />)}
     </ChakraProvider>
   );
