2d29f69bb14686a56870c9547c78b9a2658f5d7c
Jordan <1338914+Joroze@users.noreply.github.com>
2022-06-27 23:24:53 -0400

    Update site styling

--- apps/medical-spa/pages/index.tsx
+++ apps/medical-spa/pages/index.tsx
@@ -16,74 +16,99 @@ import {
   useBreakpointValue,
   useDisclosure,
   VStack,
+  forwardRef,
+  HStack,
+  Grid,
+  SimpleGrid,
+  AspectRatio,
 } from '@chakra-ui/react';
 import { FiMenu } from 'react-icons/fi';
 import { FaLinkedin } from 'react-icons/fa';
 import Link from 'next/link';
-import { useRef } from 'react';
+import React, { useRef } from 'react';
 import Image from 'next/image';
+import useSize from '@react-hook/size';
 
 export function Index() {
+  const topNavigatorContainerRef = useRef();
+  const [, topNavigatorContainerHeight] = useSize(topNavigatorContainerRef);
+
   return (
     <section>
-      <TopNavigator />
+      <TopNavigator ref={topNavigatorContainerRef} />
       <Box
-        css={{ height: ['100vh', '100dvh'] }}
+        css={{
+          height: [
+            `calc(100vh - ${topNavigatorContainerHeight}px)`,
+            `calc(100dvh - ${topNavigatorContainerHeight}px)`,
+          ],
+        }}
         width="100%"
         position="relative"
       >
         <Box padding="8" display="flex" width="100%" height="100%">
-          <Stack spacing="5" alignSelf="flex-end" color="white">
-            <Heading as="h1">
-              Take care of your mind,
-              <br /> body and soul.
-            </Heading>
-            <Text fontWeight="bold" fontSize="medium">
-              Familiarize yourself with our world-class BoTox Spa and Studio,
-              only available in NYC
-            </Text>
-            <Box>
-              <Link href="#">
-                <Button variant="link" fontWeight="semibold" color="orange.100">
-                  Book an appointment
-                </Button>
-              </Link>
+          <SimpleGrid
+            justifyItems={['center']}
+            columns={[1, 1, 2, 2]}
+            spacing={8}
+          >
+            <Box
+              borderRadius="3%"
+              width={['75%', '50%', '100%', '100%']}
+              height={['100%', '100%', '50%', '50%']}
+              overflow="hidden"
+              boxShadow="2xl"
+            >
+              <video
+                playsInline
+                style={{
+                  // filter: 'blur(.5px)',
+                  objectFit: 'cover',
+                  height: '100%',
+                  minHeight: '100%',
+                  width: '100%',
+                  minWidth: '100%',
+                }}
+                autoPlay
+                muted
+                loop
+                id="bgvid"
+                preload="auto"
+                poster="assets/images/skyline.jpg"
+              >
+                <source
+                  src="assets/videos/faces-preview.webm"
+                  type="video/webm"
+                />
+                <source
+                  src="assets/videos/faces-preview.mp4"
+                  type="video/mp4"
+                />
+              </video>
             </Box>
-          </Stack>
+            <Stack spacing="5" alignSelf="flex-end" color="white">
+              <Heading as="h1">
+                Take care of your mind,
+                <br /> body and soul.
+              </Heading>
+              <Text fontWeight="bold" fontSize="medium">
+                Familiarize yourself with our world-class BoTox Spa and Studio,
+                only available in NYC
+              </Text>
+              <Box>
+                <Link href="#">
+                  <Button
+                    variant="link"
+                    fontWeight="semibold"
+                    color="orange.100"
+                  >
+                    Book your appointment
+                  </Button>
+                </Link>
+              </Box>
+            </Stack>
+          </SimpleGrid>
         </Box>
-
-        <video
-          playsInline
-          style={{
-            zIndex: -2,
-            top: 0,
-            position: 'absolute',
-            filter: 'blur(.5px)',
-            objectFit: 'cover',
-            height: '100%',
-            minHeight: '100%',
-            width: '100%',
-            minWidth: '100%',
-          }}
-          autoPlay
-          muted
-          loop
-          id="bgvid"
-          preload="auto"
-          poster="assets/images/skyline.jpg"
-        >
-          <source src="assets/videos/city_cover.webm" type="video/webm" />
-          <source src="assets/videos/city_cover.mp4" type="video/mp4" />
-        </video>
-        <Box
-          bg="blue.100"
-          zIndex={-1}
-          top={0}
-          opacity={0.2}
-          width="full"
-          height="full"
-          position="absolute"
-        />
       </Box>
 
       <Box borderTop="1px solid gray" height="100px" position="relative">
@@ -98,7 +123,7 @@ export function Index() {
   );
 }
 
-export function TopNavigator() {
+export const TopNavigator = forwardRef((props, ref) => {
   // const isLarge = useBreakpointValue({ md: true });
 
   // const { colorMode, toggleColorMode } = useColorMode();
@@ -106,50 +131,48 @@ export function TopNavigator() {
   // const svgFilter = useColorModeValue(null, 'saturate(100)');
 
   const isDesktop = useBreakpointValue({ base: false, lg: true });
+
   return (
-    <>
-      <Flex
-        justifyContent="space-between"
-        zIndex={1}
-        px="8"
-        py="6"
-        as="nav"
-        position="fixed"
-        width="100%"
-        bgGradient="linear(to-r, #20293e, #4a637feb, #20293e)"
-        color="white"
-        boxShadow="sm"
-        alignItems="center"
-      >
-        <Link passHref href="/">
-          <a>
-            <Text fontWeight="bold" fontSize="large">
-              Adam Rosenberg PA-C
-            </Text>
-          </a>
-        </Link>
-        {isDesktop ? (
-          <ButtonGroup variant="link" size="lg" colorScheme="black" spacing="8">
-            <Link passHref href="https://www.linkedin.com/in/adamrosepa/">
-              <Box as="a" display="flex">
-                <Button aria-label="linked-in">
-                  <FaLinkedin />
-                </Button>
-              </Box>
-            </Link>
-            {['About', 'Treatments', 'Injectables', 'Location', 'Blog'].map(
-              (item) => (
-                <Button key={item}>{item}</Button>
-              )
-            )}
-          </ButtonGroup>
-        ) : (
-          <NavDrawerButton />
-        )}
-      </Flex>
-    </>
+    <Flex
+      ref={ref}
+      justifyContent="space-between"
+      px="8"
+      py="6"
+      as="nav"
+      width="100%"
+      bgGradient="linear(to-r, #20293e, #4a637feb, #20293e)"
+      color="white"
+      boxShadow="sm"
+      alignItems="center"
+    >
+      <Link passHref href="/">
+        <a>
+          <Text fontWeight="bold" fontSize="large">
+            Adam Rosenberg PA-C
+          </Text>
+        </a>
+      </Link>
+      {isDesktop ? (
+        <ButtonGroup variant="link" size="lg" colorScheme="black" spacing="8">
+          <Link passHref href="https://www.linkedin.com/in/adamrosepa/">
+            <Box as="a" display="flex">
+              <Button color="orange.100" aria-label="linked-in">
+                <FaLinkedin />
+              </Button>
+            </Box>
+          </Link>
+          {['About', 'Treatments', 'Injectables', 'Location', 'Blog'].map(
+            (item) => (
+              <Button key={item}>{item}</Button>
+            )
+          )}
+        </ButtonGroup>
+      ) : (
+        <NavDrawerButton />
+      )}
+    </Flex>
   );
-}
+});
 
 function NavDrawerButton() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -177,6 +200,18 @@ function NavDrawerButton() {
 
           <DrawerBody>
             <VStack alignItems="flex-start">
+              <Link passHref href="https://www.linkedin.com/in/adamrosepa/">
+                <Box as="a" display="flex">
+                  <Button
+                    variant="link"
+                    size="lg"
+                    minWidth="unset"
+                    aria-label="linked-in"
+                  >
+                    <FaLinkedin />
+                  </Button>
+                </Box>
+              </Link>
               {['About', 'Treatments', 'Injectables', 'Location', 'Blog'].map(
                 (item) => (
                   <Button
