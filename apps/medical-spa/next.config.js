49206a7e309ba5d244c4256802665edc48ed4f75
Jordan <1338914+Joroze@users.noreply.github.com>
2022-07-11 17:39:39 -0400

    optimize jpg

--- apps/medical-spa/next.config.js
+++ apps/medical-spa/next.config.js
@@ -8,8 +8,7 @@ const nextConfig = {
   swcMinify: true,
   poweredByHeader: false,
   images: {
-    // formats: ['image/avif', 'image/webp'],
-    formats: ['image/webp'],
+    formats: ['image/avif', 'image/webp'],
   },
   nx: {
     // Set this to true if you would like to to use SVGR
