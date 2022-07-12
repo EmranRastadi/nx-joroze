0fb24a79fe126db28a16d26f09de89e7abbd0a3b
Jordan Rosenberg <1338914+Joroze@users.noreply.github.com>
2022-07-12 11:40:26 -0400

    Remove about page

--- apps/medical-spa/pages/index.tsx
+++ apps/medical-spa/pages/index.tsx
@@ -1,94 +1,49 @@
-import {
-  Box,
-  Button,
-  Heading,
-  Stack,
-  Text,
-  SimpleGrid,
-} from '@chakra-ui/react';
-import Link from 'next/link';
-import React from 'react';
+import { Box, Flex, Heading, Text } from '@chakra-ui/react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { getLastSegmentInPath } from '../lib/routes';
 
 export function Index() {
+  const router = useRouter();
+
   return (
-    <Box as="section">
-      <Box
-        css={{
-          minHeight: ['calc(100vh - 75px)', 'calc(100dvh - 75px)'],
-        }}
-        width="100%"
-      >
-        <Box padding="8" display="flex" width="100%" minHeight="inherit">
-          <SimpleGrid
-            justifyItems="center"
-            flexGrow={1}
-            columns={[1, 1, 2, 2]}
-            spacing={8}
+    <Box color="white" as="section">
+      <Flex gap={6} padding="8" flexDir="column">
+        <Flex gap={2} flexDir="column">
+          <Heading textTransform="capitalize">
+            {getLastSegmentInPath(router.pathname)}
+          </Heading>
+          <Text>
+            Adam Rosenberg is a Board Certified Physician Assistant based in
+            NYC.
+          </Text>
+          <Text>
+            Originally surgically and procedurally trained, he aims for precise
+            and detailed results cosmetically.
+          </Text>
+          <Text>
+            Adam is experienced with Botox injections for aesthetic
+            results/facial balancing as well as medical indications including
+            chronic migraines, jaw pain and excessive sweating.
+          </Text>
+        </Flex>
+        <Flex justifyContent="flex-end">
+          <Box
+            isolation="isolate"
+            display="flex"
+            overflow="hidden"
+            borderRadius="4px"
+            boxShadow="2xl"
           >
-            <Box
-              alignSelf={['center', 'center', 'flex-start', 'flex-start']}
-              isolation="isolate"
-              borderRadius={['20px', '15px', '10px']}
-              width={['100%', '75%', '100%', '100%']}
-              height={['90%', '80%', '50%', '50%']}
-              overflow="hidden"
-              boxShadow="2xl"
-            >
-              <video
-                playsInline
-                style={{
-                  objectFit: 'cover',
-                  height: '100%',
-                  minHeight: '100%',
-                  width: '100%',
-                  minWidth: '100%',
-                }}
-                autoPlay
-                muted
-                loop
-                id="bgvid"
-                preload="auto"
-              >
-                <source
-                  src="assets/videos/faces-preview.webm"
-                  type="video/webm"
-                />
-                <source
-                  src="assets/videos/faces-preview.mp4"
-                  type="video/mp4"
-                />
-              </video>
-            </Box>
-            <Box
-              position="relative"
-              height="50%"
-              alignSelf={[null, null, 'flex-end']}
-            >
-              <Stack spacing="5" color="white">
-                <Heading as="h1">
-                  Take care of your mind,
-                  <br /> body and soul.
-                </Heading>
-                <Text fontWeight="bold" fontSize="medium">
-                  Familiarize yourself with our world-class BoTox Spa and
-                  Studio, only available in NYC
-                </Text>
-                <Box>
-                  <Link href="#">
-                    <Button
-                      variant="link"
-                      fontWeight="semibold"
-                      color="orange.100"
-                    >
-                      Book your appointment
-                    </Button>
-                  </Link>
-                </Box>
-              </Stack>
-            </Box>
-          </SimpleGrid>
-        </Box>
-      </Box>
+            <Image
+              width="450px"
+              height="450px"
+              alt="action shot"
+              src="/assets/images/actionshot.jpg"
+            />
+          </Box>
+        </Flex>
+      </Flex>
     </Box>
   );
 }
