ae8f4e114ea3e5671a8a14caed5d41e66e7699ca
Jordan <1338914+Joroze@users.noreply.github.com>
2022-07-12 01:29:19 -0400

    Add in img preloading and blur

---
+++ apps/medical-spa/include_sharp/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  async onBuild({ netlifyConfig }) {
+    const fn = netlifyConfig.functions['___netlify-handler'];
+
+    fn.included_files = fn.included_files.filter(
+      (f) => f !== '!node_modules/sharp/**/*'
+    );
+  },
+};
